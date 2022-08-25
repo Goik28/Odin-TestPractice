1a1f7ecb50ed17e0fd64374a25811d646c996a86
Goik <sbabinski@outlook.com>
2022-08-25 13:12:54 -0300

    Complete exercises 4-5
    Write test and function to exercises 4 and 5.

--- exercises.js
+++ exercises.js
@@ -1,7 +1,4 @@
-/*Write tests for the following, and then make the tests pass!
-
-4- A caesarCipher function that takes a string and returns it with each character “shifted”.
-5- An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.*/
+//Write tests for the following, and then make the tests pass!
 
 //1- A capitalize function that takes a string and returns it with the first character capitalized.
 export function capitalize(string) {
@@ -38,4 +35,47 @@ export const calculator = () => {
     return a * b;
   };
   return { sum, subtract, divide, multiply };
+};
+
+//4- A caesarCipher function that takes a string and returns it with each character “shifted”.
+export function caesarCipher(string, offset) {
+  let shiftedString = "";
+  let re = /[a-z]/;
+  for (let i = 0; i < string.length; i++) {
+    if (re.test(string.charAt(i)))
+      shiftedString += String.fromCharCode(
+        ((string.charCodeAt(i) - 97 + offset) % 26) + 97
+      );
+    else shiftedString += string.charAt(i);
+  }
+  return shiftedString;
+}
+
+/*5- An analyzeArray function that takes an array of numbers and returns an object
+with the following properties: average, min, max, and length.*/
+export const analyzeArray = () => {
+  const average = (array) => {
+    let sum = 0;
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      sum += element;
+    }
+    return sum / array.length;
+  };
+
+  const min = (array) => {
+    let arraySorted = array.sort((a, b) => a - b);
+    return arraySorted[0];
+  };
+
+  const max = (array) => {
+    let arraySorted = array.sort((a, b) => a - b);
+    return arraySorted[arraySorted.length - 1];
+  };
+
+  const length = (array) => {
+    return array.length;
+  };
+
+  return { average, min, max, length };
 };
--- exercises.test.js
+++ exercises.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString, calculator } from "./exercises.js";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./exercises.js";
 
 test("1- Capitalize the first letter.", () => {
   expect(capitalize("practice")).toBe("Practice");
@@ -8,12 +14,20 @@ test("2- Reverse a string.", () => {
   expect(reverseString("Practice")).toBe("ecitcarP");
 });
 
-test("3- Calculator object with basic operations",
-  () => {
-    expect(calculator().sum(4, 4)).toBe(8);
-    expect(calculator().subtract(4, 4)).toBe(0);
-    expect(calculator().divide(4, 4)).toBe(1);
-    expect(calculator().multiply(4, 4)).toBe(16);
-  });
+test("3- Calculator object with basic operations.", () => {
+  expect(calculator().sum(4, 4)).toBe(8);
+  expect(calculator().subtract(4, 4)).toBe(0);
+  expect(calculator().divide(4, 4)).toBe(1);
+  expect(calculator().multiply(4, 4)).toBe(16);
+});
 
-  
+test("4- Caesar cipher a string by an offset.", () => {
+  expect(caesarCipher("practice is secret", 3)).toBe("sudfwlfh lv vhfuhw");
+});
+
+test("5- An analyze Array object with average, min, max and length for the array.",()=>{
+    expect(analyzeArray().average([1,2,3,4,5,6,7,8,9])).toBe(5);
+    expect(analyzeArray().min([5,2,3,4,1,6,7,8,9])).toBe(1);
+    expect(analyzeArray().max([1,2,3,4,9,6,7,8,5])).toBe(9);
+    expect(analyzeArray().length([1,2,3,4,5,6,7,8,9])).toBe(9);
+});
