b554cd6fcdf269c1aa649694cb76bca8b6b66eee
Goik <sbabinski@outlook.com>
2022-08-24 23:57:48 -0300

    Develop exercises 1 to 3 and its tests
    Include babel config.
    Write tests to exercises 1 to 3.
    Write function to exercises 1 to 3.

--- exercises.js
+++ exercises.js
@@ -1,11 +1,41 @@
 /*Write tests for the following, and then make the tests pass!
-1- A capitalize function that takes a string and returns it with the first character capitalized.
-2- A reverseString function that takes a string and returns it reversed.
-3- A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+
 4- A caesarCipher function that takes a string and returns it with each character “shifted”.
 5- An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.*/
 
+//1- A capitalize function that takes a string and returns it with the first character capitalized.
 export function capitalize(string) {
-  let capitalizedString;
-  return capitalizedString;
+  let firstLetter = string[0];
+  firstLetter = firstLetter.toUpperCase();
+  string = string.substring(1);
+  string = firstLetter.concat(string);
+  return string;
 }
+
+//2- A reverseString function that takes a string and returns it reversed.
+export function reverseString(string) {
+  let array = Array.from(string);
+  array = array.reverse();
+  string = array.toString();
+  string = string.replaceAll(",", "");
+  return string;
+}
+
+/*3- A calculator object that contains functions for the basic operations: add, subtract,
+divide, and multiply. Each of these functions should take two numbers and return the correct
+calculation.*/
+export const calculator = () => {
+  const sum = (a, b) => {
+    return a + b;
+  };
+  const subtract = (a, b) => {
+    return a - b;
+  };
+  const divide = (a, b) => {
+    return a / b;
+  };
+  const multiply = (a, b) => {
+    return a * b;
+  };
+  return { sum, subtract, divide, multiply };
+};
--- exercises.test.js
+++ exercises.test.js
@@ -1,5 +1,19 @@
-import { capitalize } from "./exercises.js";
+import { capitalize, reverseString, calculator } from "./exercises.js";
 
-test("Capitalize the first letter: practice to Practice", ()=>{
-    expect(capitalize("practice")).toBe(3);
-})
+test("1- Capitalize the first letter.", () => {
+  expect(capitalize("practice")).toBe("Practice");
+});
+
+test("2- Reverse a string.", () => {
+  expect(reverseString("Practice")).toBe("ecitcarP");
+});
+
+test("3- Calculator object with basic operations",
+  () => {
+    expect(calculator().sum(4, 4)).toBe(8);
+    expect(calculator().subtract(4, 4)).toBe(0);
+    expect(calculator().divide(4, 4)).toBe(1);
+    expect(calculator().multiply(4, 4)).toBe(16);
+  });
+
+  
