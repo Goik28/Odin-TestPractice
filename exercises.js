0f2315e417128019117ec57611f53d42398ea95d
Goik <sbabinski@outlook.com>
2022-08-24 21:03:36 -0300

    Init project files
    Create files, write README.MD.
    Install jest.

---
+++ exercises.js
@@ -0,0 +1,11 @@
+/*Write tests for the following, and then make the tests pass!
+1- A capitalize function that takes a string and returns it with the first character capitalized.
+2- A reverseString function that takes a string and returns it reversed.
+3- A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+4- A caesarCipher function that takes a string and returns it with each character “shifted”.
+5- An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.*/
+
+export function capitalize(string) {
+  let capitalizedString;
+  return capitalizedString;
+}
---
+++ exercises.test.js
@@ -0,0 +1,5 @@
+import { capitalize } from "./exercises.js";
+
+test("Capitalize the first letter: practice to Practice", ()=>{
+    expect(capitalize("practice")).toBe(3);
+})
